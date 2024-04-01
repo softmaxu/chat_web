6bfd3f89d46b5bc018b6b37b9c2528a9159ae2a0
nb <nbs412004@outlook.com>
2024-04-01 19:29:25 +0800

    feat 显示ws连接状态

--- src/components/ChatInput/ChatInput.js
+++ src/components/ChatInput/ChatInput.js
@@ -2,15 +2,15 @@ import React, { useState, useCallback } from 'react';
 import './ChatInput.css'; // 确保CSS文件的路径正确
 import useWebSocket from '../../utils/websocketService'; // 引入自定义的useWebSocket Hook
 
-const ChatInput = ({messages, pageName, displaySentmessage,onReceiveMessage, onFileUpload }) => {
-  console.log("ChatInput",pageName)
+const ChatInput = ({ messages, pageName, displaySentmessage, onReceiveMessage, onFileUpload }) => {
+  console.log("ChatInput", pageName)
   const [inputValue, setInputValue] = useState('');
 
   const { sendMessage, isConnected } = useWebSocket('ws://10.82.77.104:8081', onReceiveMessage);
 
   const handleSendMessage = () => {
-    const newMessage={ text: inputValue, type: 'sent', page:pageName }
-    const messageString = JSON.stringify([...messages,newMessage]);
+    const newMessage = { text: inputValue, type: 'sent', page: pageName }
+    const messageString = JSON.stringify([...messages, newMessage]);
     sendMessage(messageString);
     displaySentmessage(newMessage);
     setInputValue(''); // 清空输入框
@@ -30,23 +30,28 @@ const ChatInput = ({messages, pageName, displaySentmessage,onReceiveMessage, onF
   };
 
   return (
-<div className="chat-input">
-  <textarea
-    value={inputValue}
-    onChange={(e) => setInputValue(e.target.value)}
-    placeholder="请输入......"
-    onKeyDown={handleKeyDown}
-  ></textarea>
-  <button onClick={()=>handleSendMessage()} disabled={!isConnected}>发送</button>
-  <label className="file-upload-btn">
-    <i className="fas fa-paperclip"></i> {/* 使用Font Awesome图标 */}
-    <input
-      type="file"
-      onChange={handleFileChange}
-      style={{ display: 'none' }} // 完全隐藏
-    />
-  </label>
-</div>
+    <>
+      <div className="websocket-status">
+        {isConnected ? '✔️ 已连接到服务器' : '❌ 与服务器连接断开'}
+      </div>
+      <div className="chat-input">
+        <textarea
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+          placeholder="请输入......"
+          onKeyDown={handleKeyDown}
+        ></textarea>
+        <button onClick={() => handleSendMessage()} disabled={!isConnected}>发送</button>
+        <label className="file-upload-btn">
+          <i className="fas fa-paperclip"></i> {/* 使用Font Awesome图标 */}
+          <input
+            type="file"
+            onChange={handleFileChange}
+            style={{ display: 'none' }} // 完全隐藏
+          />
+        </label>
+      </div>
+    </>
   );
 };
 
